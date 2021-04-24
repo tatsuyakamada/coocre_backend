e46f55237ee6bc3a362a75a99c2e69dc1750f356
[tatsuyakamada] <t.kamada1151@gmail.com>
2021-04-25 08:51:06 +0900

    Fixed DishStuff validation

--- app/models/dish_stuff.rb
+++ app/models/dish_stuff.rb
@@ -4,7 +4,9 @@ class DishStuff < ApplicationRecord
 
   enum category: { essential: 0, desireble: 1, optional: 2 }
 
+  validate :duplicate?
+
   def duplicate?
-    errors.add(:dish_id, 'duplicated!') if Menu.where(dish_id: dish_id, stuff_id: stuff_id).where.not(id: id).count.positive?
+    errors.add(:dish_id, 'duplicated!') if DishStuff.where(dish_id: dish_id, stuff_id: stuff_id).where.not(id: id).count.positive?
   end
 end
